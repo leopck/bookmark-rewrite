e5fe2ee370d25567628a0d4ce60bb48c2e87892d
leopck <stanley.phoong@gmail.com>
2015-12-13 16:20:03 +0800

    Correction

--- script.js
+++ script.js
@@ -27,14 +27,14 @@ else if(document.getElementsByClassName("cprice-area").length != 0)
   custom_pochi_price_length  =  document.getElementsByClassName("cprice-area").length;
   custom_pochi_price_label  =  document.getElementsByClassName("cprice-area");
   for(var i = 0;i<custom_pochi_price_length;i++){
-      if(document.getElementsByClassName("cprice-area")[i].getElementsByTagName("span")[0].innerHTML != '<font size="1">RM</font>')
+      if(document.getElementsByClassName("cprice-area")[i].getElementsByClassName("symbol")[0].innerHTML != '<font size="1">RM</font>')
       {
           custom_pochi_current_value = custom_pochi_price_label[i].getElementsByClassName("c-price")[0].innerHTML;
           custom_pochi_previous_value = custom_pochi_current_value;
           custom_pochi_current_value = Math.round((custom_pochi_current_value*100/custom_pochi_exchange_rate))/100;
           console.log(custom_pochi_current_value);
-          custom_pochi_price_label[i].getElementsByTagName("c-price")[0].innerHTML =  '<font size="3">'+ custom_pochi_current_value + '</font>' + '<font size="1">(&yen' + custom_pochi_previous_value + ')</font>';
-          document.getElementsByClassName("cprice-area")[i].getElementsByTagName("span")[0].innerHTML = '<font size="1">RM</font>';
+          custom_pochi_price_label[i].getElementsByClassName("c-price")[0].innerHTML =  '<font size="3">'+ custom_pochi_current_value + '</font>' + '<font size="1">(&yen' + custom_pochi_previous_value + ')</font>';
+          document.getElementsByClassName("cprice-area")[i].getElementsByClassName("symbol")[0].innerHTML = '<font size="1">RM</font>';
       }
   }
 }
