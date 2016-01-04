acf0fce2b0826f36b3010bf8e4c0c53f50c1cdd0
Stanley <leopck@users.noreply.github.com>
2016-01-04 20:00:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ if(document.getElementsByClassName("g_price").length != 0)
     {
       if(document.getElementsByClassName("g_price")[i].getElementsByTagName("span")[0].innerHTML != '<font size="1">RM</font>')
       {
-          custom_pochi_current_value = custom_pochi_price_label[i].getElementsByTagName("strong")[0].innerHTML;
-          custom_pochi_previous_value = custom_pochi_current_value;
-          custom_pochi_current_value = Math.round((custom_pochi_current_value*100/custom_pochi_exchange_rate))/100;
-          console.log(custom_pochi_current_value);
-          custom_pochi_price_label[i].getElementsByTagName("strong")[0].innerHTML =  '<font size="3">'+ custom_pochi_current_value + '</font>' + '<font size="1">(&yen' + custom_pochi_previous_value + ')</font>';
-          document.getElementsByClassName("g_price")[i].getElementsByTagName("span")[0].innerHTML = '<font size="1">RM</font>';
+        console.log("Changing the g_price");
+          // custom_pochi_current_value = custom_pochi_price_label[i].getElementsByTagName("strong")[0].innerHTML;
+          // custom_pochi_previous_value = custom_pochi_current_value;
+          // custom_pochi_current_value = Math.round((custom_pochi_current_value*100/custom_pochi_exchange_rate))/100;
+          // console.log(custom_pochi_current_value);
+          // custom_pochi_price_label[i].getElementsByTagName("strong")[0].innerHTML =  '<font size="3">'+ custom_pochi_current_value + '</font>' + '<font size="1">(&yen' + custom_pochi_previous_value + ')</font>';
+          // document.getElementsByClassName("g_price")[i].getElementsByTagName("span")[0].innerHTML = '<font size="1">RM</font>';
       }
     }
   }
