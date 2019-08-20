7e47ffe0e27c1048800124d4f4ae3ab55a1fc31a
Stanley Phoong <stanley.phoong@gmail.com>
2019-08-20 10:08:23 +0800

    added webnovel and restructure and rename repo
    Signed-off-by: Stanley Phoong <stanley.phoong@gmail.com>

---
+++ webnovel/bookmark.js
@@ -0,0 +1,1 @@
+javascript:(function(){ var%20script=document.createElement('script'); script.type='text/javascript'; script.src='http://iotpush.comli.com/script.js'; document.getElementsByTagName('head')[0].appendChild(script); })();
---
+++ webnovel/script.js
@@ -0,0 +1,40 @@
+var custom_pochi_exchange_rate  =  1.60;
+var custom_pochi_price_length;
+var custom_pochi_price_label;
+var custom_pochi_current_value;
+var custom_pochi_previous_value;
+//alert(custom_pochi_price_length);
+
+if(document.getElementsByClassName("g_price").length != 0)
+{
+  custom_pochi_price_length  =  document.getElementsByClassName("g_price").length;
+  custom_pochi_price_label  =  document.getElementsByClassName("g_price");
+  for(var i = 0;i<custom_pochi_price_length;i++){
+      if(document.getElementsByClassName("g_price")[i].getElementsByTagName("span")[0].innerHTML != '<font size="1">RM</font>')
+      {
+          custom_pochi_current_value = custom_pochi_price_label[i].getElementsByTagName("strong")[0].innerHTML;
+          custom_pochi_previous_value = custom_pochi_current_value;
+          custom_pochi_current_value = Math.round((custom_pochi_current_value*100/custom_pochi_exchange_rate))/100;
+          console.log(custom_pochi_current_value);
+          custom_pochi_price_label[i].getElementsByTagName("strong")[0].innerHTML =  '<font size="3">'+ custom_pochi_current_value + '</font>' + '<font size="1">(&yen' + custom_pochi_previous_value + ')</font>';
+          document.getElementsByClassName("g_price")[i].getElementsByTagName("span")[0].innerHTML = '<font size="1">RM</font>';
+      }
+  }
+}
+
+else if(document.getElementsByClassName("cprice-area").length != 0)
+{
+  custom_pochi_price_length  =  document.getElementsByClassName("cprice-area").length;
+  custom_pochi_price_label  =  document.getElementsByClassName("cprice-area");
+  for(var i = 0;i<custom_pochi_price_length;i++){
+      if(document.getElementsByClassName("cprice-area")[i].getElementsByClassName("symbol")[0].innerHTML != '<font size="1">RM</font>')
+      {
+          custom_pochi_current_value = custom_pochi_price_label[i].getElementsByClassName("c-price")[0].innerHTML;
+          custom_pochi_previous_value = custom_pochi_current_value;
+          custom_pochi_current_value = Math.round((custom_pochi_current_value*100/custom_pochi_exchange_rate))/100;
+          console.log(custom_pochi_current_value);
+          custom_pochi_price_label[i].getElementsByClassName("c-price")[0].innerHTML =  '<font size="3">'+ custom_pochi_current_value + '</font>' + '<font size="1">(&yen' + custom_pochi_previous_value + ')</font>';
+          document.getElementsByClassName("cprice-area")[i].getElementsByClassName("symbol")[0].innerHTML = '<font size="1">RM</font>';
+      }
+  }
+}
